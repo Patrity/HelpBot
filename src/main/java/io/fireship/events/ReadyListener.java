24f40bd631c74770ea7b9a163e24748c3d4899b4
Tony Costanzo <tonycos92@gmail.com>
2022-10-25 14:49:36 -0500

    Correct event checking on startup

--- src/main/java/io/fireship/events/ReadyListener.java
+++ src/main/java/io/fireship/events/ReadyListener.java
@@ -1,12 +1,15 @@
 package io.fireship.events;
 
 import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 
 public class ReadyListener implements EventListener {
 
     @Override
     public void onEvent(GenericEvent event) {
-        System.out.println("Fireship Helpbot is ready!");
+        if (event instanceof ReadyEvent) {
+            System.out.println("Fireship Helpbot is ready!");
+        }
     }
 }
