c64436675babb8c7241d21c73c759be207da759c
Tony Costanzo <tonycos92@gmail.com>
2023-06-15 19:09:18 -0500

    Add help command

--- src/main/java/io/fireship/commands/impl/Help.java
+++ src/main/java/io/fireship/commands/impl/Help.java
@@ -1,11 +1,26 @@
 package io.fireship.commands.impl;
 
 import io.fireship.commands.Command;
+import io.fireship.commands.CommandEnum;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public class Help implements Command {
     @Override
     public void onCommand(SlashCommandInteractionEvent event) {
-        event.reply("Help command coming soon!").setEphemeral(true).queue();
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setColor(0xe69138);
+        embed.setTitle("Helpbot Commands");
+        embed.setFooter("https://fireship.io/");
+        for (CommandEnum commands : CommandEnum.values()) {
+            if(!commands.isModeratorOnly())
+                embed.addField("/"+commands.getName(), commands.getDescription(), true);
+        }
+        embed.addField("__Moderator Commands__", "The commands below are only available to moderators.", false);
+        for (CommandEnum commands : CommandEnum.values()) {
+            if(commands.isModeratorOnly())
+                embed.addField("/"+commands.getName(), commands.getDescription(), true);
+        }
+        event.replyEmbeds(embed.build()).setEphemeral(true).queue();
     }
 }
