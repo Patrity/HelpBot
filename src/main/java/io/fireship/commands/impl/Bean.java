f61a24fc6c439b7ab7101dfa189d5093bcfd6c0a
pixelafk <75451291+pixelafk@users.noreply.github.com>
2023-08-05 21:52:47 +0200

    Add escape for `_` in Beaned field.

--- src/main/java/io/fireship/commands/impl/Bean.java
+++ src/main/java/io/fireship/commands/impl/Bean.java
@@ -4,12 +4,12 @@ import io.fireship.commands.Command;
 import io.fireship.commands.HasOptions;
 import io.fireship.model.Option;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Objects;
@@ -17,51 +17,48 @@ import java.util.Objects;
 public class Bean implements Command, HasOptions {
     @Override
     public void onCommand(SlashCommandInteractionEvent event) {
-        Member moderator = event.getMember();
+        User moderator = event.getUser();
         User beaned = Objects.requireNonNull(event.getOption("user")).getAsUser();
-        var reason = event.getOption("reason");
-
-        assert moderator != null;
-
-        var description = new StringBuilder();
-
-        if (reason != null) {
-            description.append(String.format(
-                    "<:activity:1137406861855969310> **Reason:** %s\n",
-                    reason.getAsString()
-            ));
-        } else {
-            description.append("<:activity:1137406861855969310> **Reason:** No reason.\n");
-        }
-
-        description.append(String.format(
-                "<:trustedAdmin:1137406869535735850> **Moderator:** %s<:whitelistUser:1137406878033387610><:whitelistInvite:1137406875302891680><:whitelistChannel:1137406873868451901><:upvoter:1137406871435759736><:potentialDanger:1137406863349121036>",
-                moderator.getAsMention()
-        ));
-
-        var formattedBeaned = new StringBuilder("<:space:1137406865299488798><:success:1137406866251591731> ");
-
-        formattedBeaned.append(beaned.getName());
-
-        if (!beaned.getDiscriminator().equals("0000")) {
-            formattedBeaned.append("#")
-                    .append(beaned.getDiscriminator());
-        }
-
-        formattedBeaned.append(" [`").append(beaned.getId()).append("`]");
+        var opt = event.getOption("reason");
+        var reason = opt == null ? null : opt.getAsString();
 
         MessageEmbed response = new EmbedBuilder()
                 .setColor(Role.DEFAULT_COLOR_RAW)
                 .setTitle("Bean Result:")
-                .setDescription(description)
+                .setDescription(getDescriptionFor(moderator, reason))
                 .addField(
                     "Beaned:",
-                    formattedBeaned.toString(),
+                    formatName(beaned).replace("_", "\\_"),
                     false
                 )
                 .build();
 
         event.replyEmbeds(response).queue();
+    }
+
+    private String getDescriptionFor(@NotNull User moderator, String reason) {
+        var description = "<:activity:1137406861855969310> **Reason:** %s\n" +
+                "<:trustedAdmin:1137406869535735850> **Moderator:** %s" +
+                "<:whitelistUser:1137406878033387610><:whitelistInvite:1137406875302891680><:whitelistChannel:1137406873868451901><:upvoter:1137406871435759736><:potentialDanger:1137406863349121036>";
+
+        reason = reason == null ? "No reason." : reason;
+
+        return description.formatted(reason, moderator.getAsMention());
+    }
+
+    @NotNull
+    private String formatName(User beaned) {
+        var discriminator = beaned.getDiscriminator();
+        var formatted = new StringBuilder("<:space:1137406865299488798><:success:1137406866251591731> %s");
+
+        if (!discriminator.equals("0000")) {
+            formatted.append("#")
+                    .append(discriminator);
+        }
+
+        formatted.append(" [`%s`]");
+
+        return formatted.toString().formatted(beaned.getName(), beaned.getId());
     }
 
     @Override
