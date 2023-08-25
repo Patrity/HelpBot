dde788373dba594164b01c6a70866132191cfb19
adambrangenberg <adambrangenberg@proton.me>
2023-08-25 22:36:57 +0200

    Defining channels

--- src/main/java/io/fireship/commands/CommandEnum.java
+++ src/main/java/io/fireship/commands/CommandEnum.java
@@ -2,26 +2,34 @@ package io.fireship.commands;
 
 import io.fireship.commands.impl.*;
 import io.fireship.model.Option;
-
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
+
+// Using normal names to indentify easier and account for channel replacement; Unlikely to rename?
+interface Channel {
+    String[] help = { "frontend-chat", "backend-chat", "machine-learning-chat", "game-dev-chat", "mathematics-chat", "systems-chat", "help"};
+    String[] spam = { "bot-spam" };
+    String[] all =  Stream.concat(Arrays.stream(help), Arrays.stream(spam)).toArray(String[]::new);
+}
 
 public enum CommandEnum {
-    HELP("help", "Displays a message on how to use this bot.", false, new Help()),
-    PROHELP("prohelp", "Shows users how to unlock Fireship pro perks in Discord", false, new ProHelp()),
-    CODE("code", "Shows users how to share code using markdown", false, new Code()),
-    THANK("thank", "Thanks a user for helping", false, new Thank()),
-    SETUP_LANGUAGE_ROLES("setup-tool-roles", "Sets up the tool roles message", true, new SetupToolRoles()),
-    BROWSER("browser", "Common browser issues", false, new Browser()),
-    RESPONSE("response", "Fireship response", true, new Response()),
-    BEAN("bean", "Bean a user", true, new Bean()),
-    PING("ping", "Returns round trip time", false, new Ping());
+    HELP("help", "Displays a message on how to use this bot.", false, new Help(), Channel.spam),
+    PROHELP("prohelp", "Shows users how to unlock Fireship pro perks in Discord", false, new ProHelp(), Channel.spam),
+    CODE("code", "Shows users how to share code using markdown", false, new Code(), Channel.all),
+    THANK("thank", "Thanks a user for helping", false, new Thank(), Channel.help),
+    SETUP_LANGUAGE_ROLES("setup-tool-roles", "Sets up the tool roles message", true, new SetupToolRoles(), null),
+    BROWSER("browser", "Common browser issues", false, new Browser(), Channel.spam),
+    RESPONSE("response", "Fireship response", true, new Response(), null),
+    BEAN("bean", "Bean a user", true, new Bean(), null),
+    PING("ping", "Returns round trip time", false, new Ping(), Channel.spam);
 
     private final String name, description;
     private final boolean moderatorOnly;
     private final Command command;
 
-    CommandEnum(String name, String description, boolean moderatorOnly, Command command) {
+    CommandEnum(String name, String description, boolean moderatorOnly, Command command, String[] allowedChannels) {
         this.name = name;
         this.description = description;
         this.moderatorOnly = moderatorOnly;
