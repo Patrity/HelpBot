2fb4651f1d262e08304cfab7dcf9f27fc87727c0
adambrangenberg <adambrangenberg@proton.me>
2023-08-27 20:17:31 +0200

    Making ping mod only

--- src/main/java/io/fireship/commands/CommandEnum.java
+++ src/main/java/io/fireship/commands/CommandEnum.java
@@ -1,35 +1,31 @@
 package io.fireship.commands;
 
 import io.fireship.commands.impl.*;
-import io.fireship.data.Channel;
 import io.fireship.model.Option;
 
 import java.util.List;
 import java.util.Optional;
 
-
 public enum CommandEnum {
-    HELP("help", "Displays a message on how to use this bot.", false, new Help(), Channel.spam),
-    PROHELP("prohelp", "Shows users how to unlock Fireship pro perks in Discord", false, new ProHelp(), Channel.spam),
-    CODE("code", "Shows users how to share code using markdown", false, new Code(), Channel.all),
-    THANK("thank", "Thanks a user for helping", false, new Thank(), Channel.help),
-    SETUP_LANGUAGE_ROLES("setup-tool-roles", "Sets up the tool roles message", true, new SetupToolRoles(), Channel.all),
-    BROWSER("browser", "Common browser issues", false, new Browser(), Channel.spam),
-    RESPONSE("response", "Fireship response", true, new Response(), Channel.all),
-    BEAN("bean", "Bean a user", true, new Bean(), Channel.all),
-    PING("ping", "Returns round trip time", false, new Ping(), Channel.spam);
+    HELP("help", "Displays a message on how to use this bot.", false, new Help()),
+    PROHELP("prohelp", "Shows users how to unlock Fireship pro perks in Discord", false, new ProHelp()),
+    CODE("code", "Shows users how to share code using markdown", false, new Code()),
+    THANK("thank", "Thanks a user for helping", false, new Thank()),
+    SETUP_LANGUAGE_ROLES("setup-tool-roles", "Sets up the tool roles message", true, new SetupToolRoles()),
+    BROWSER("browser", "Common browser issues", false, new Browser()),
+    RESPONSE("response", "Fireship response", true, new Response()),
+    BEAN("bean", "Bean a user", true, new Bean()),
+    PING("ping", "Returns round trip time", true, new Ping());
 
     private final String name, description;
     private final boolean moderatorOnly;
     private final Command command;
-    private final List<String> allowedChannels;
 
-    CommandEnum(String name, String description, boolean moderatorOnly, Command command, List<String> allowedChannels) {
+    CommandEnum(String name, String description, boolean moderatorOnly, Command command) {
         this.name = name;
         this.description = description;
         this.moderatorOnly = moderatorOnly;
         this.command = command;
-        this.allowedChannels = allowedChannels;
     }
 
     //misc getters/setters
@@ -44,9 +40,6 @@ public enum CommandEnum {
     }
     public Command getCommand() {
         return this.command;
-    }
-    public List<String> getAllowedChannels() {
-        return this.allowedChannels;
     }
     public Optional<List<Option>> getOptions() {
         var command = getCommand();
